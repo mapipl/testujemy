0c8b62f04eb5408771c3dea45469983a8aa7ff08
maciek <maciek_p@interia.pl>
2020-07-11 14:08:02 +0200

    for each test

--- src/main/java/testing/zadanie02.java
+++ src/main/java/testing/zadanie02.java
@@ -1,4 +1,20 @@
 package testing;
 
 public class zadanie02 {
+
+    public static String makeShoppingList(String[] shoppingList) {
+        String list = "";
+        for (String name : shoppingList) {
+            list += name;
+            list += ", ";
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"ser", "bula", "szynka"};
+        String shoppingList = makeShoppingList(names);
+        System.out.println(shoppingList);
+    }
 }
